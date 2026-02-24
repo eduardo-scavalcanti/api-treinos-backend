2c02fceca19ce5f6dba24820aafa697386d546b6
Eduardo Cavalcanti <eduardoscavalcantij@gmail.com>
2026-02-24 13:11:59 -0300

    feat: cria viewsets para expor as operacoes de CRUD na api

--- treinos/views.py
+++ treinos/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Exercise, WorkoutTemplate, WorkoutSession, ExerciseLog
+from .serializers import ExerciseSerializer, WorkoutTemplateSerializer, WorkoutSessionSerializer, ExerciseLogSerializer
+
+
+class ExerciseViewSet(viewsets.ModelViewSet):
+    queryset = Exercise.objects.all()
+
+    serializer_class = ExerciseSerializer
+
+
+class WorkoutTemplateViewSet(viewsets.ModelViewSet):
+    queryset = WorkoutTemplate.objects.all()
+
+    serializer_class = WorkoutTemplateSerializer
+
+
+class WorkoutSessionViewSet(viewsets.ModelViewSet):
+    queryset = WorkoutSession.objects.all()
+
+    serializer_class = WorkoutSessionSerializer
+
+
+class ExerciseLogViewSet(viewsets.ModelViewSet):
+    queryset = ExerciseLog.objects.all()
+
+    serializer_class = ExerciseLogSerializer
