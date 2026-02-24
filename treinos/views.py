39d7e3c2844741e5e990c46cde4f8183761bd962
Eduardo Cavalcanti <eduardoscavalcantij@gmail.com>
2026-02-24 15:10:15 -0300

    feat: adiciona permissoes de autenticacao nas rotas de api

--- treinos/views.py
+++ treinos/views.py
@@ -1,29 +1,29 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Exercise, WorkoutTemplate, WorkoutSession, ExerciseLog
 from .serializers import ExerciseSerializer, WorkoutTemplateSerializer, WorkoutSessionSerializer, ExerciseLogSerializer
 
 
 class ExerciseViewSet(viewsets.ModelViewSet):
     queryset = Exercise.objects.all()
-
     serializer_class = ExerciseSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class WorkoutTemplateViewSet(viewsets.ModelViewSet):
     queryset = WorkoutTemplate.objects.all()
-
     serializer_class = WorkoutTemplateSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class WorkoutSessionViewSet(viewsets.ModelViewSet):
     queryset = WorkoutSession.objects.all()
-
     serializer_class = WorkoutSessionSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class ExerciseLogViewSet(viewsets.ModelViewSet):
     queryset = ExerciseLog.objects.all()
-
     serializer_class = ExerciseLogSerializer
+    permission_classes = [permissions.IsAuthenticated]
