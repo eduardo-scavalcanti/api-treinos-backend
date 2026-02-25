ee9e5dc5a92a939e6209ad2064df786a1bb7b091
Eduardo Cavalcanti <eduardoscavalcantij@gmail.com>
2026-02-25 15:05:39 -0300

    feat: implementa paginacao global para api

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 # Visual settings of the documentation
